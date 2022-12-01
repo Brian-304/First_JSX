6986cce49da1b1f54bb0cf40eed994341954a3ba
BrianT <304@DESKTOP-OBFHSVF>
2022-11-30 22:30:39 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Brian from './components/brian';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Brian/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/brian.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Brian extends React.Component {
+    render() {
+        return(
+            <div>
+    <h1>Hello Dojo!</h1>
+        <p>
+        THings I need to do:
+        </p>
+
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the Dogs</li>
+        </div>
+        )
+    }
+}
+
+export default Brian;
+    
